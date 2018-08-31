0c6f3ac5ff517b2b58be740351653a7dbcf26927
shreyanshjn <shrey.umt2017@iitr.ac.in>
2018-08-31 13:36:07 +0530

    uploaded new website

--- src/js/animation.js
+++
@@ -1,8 +0,0 @@
-$(".scrollTo").on('click', function(e) {
-     e.preventDefault();
-     var target = $(this).attr('href');
-     $('html, body').animate({
-       scrollTop: ($(target).offset().top)
-     }, 2000);
-  });
-
